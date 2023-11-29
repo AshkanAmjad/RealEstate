1cd1f26529c0e1eff1aad24056707fcc49a35672
AshkanAmjad <amjad.ashkan@gmail.com>
2023-11-29 23:08:43 +0330

    Personalize registration, login and logout

---
+++ RealEstate/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace RealEstate.Models.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Phone number")]
+        public string phoneNumber { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+        public string? ReturnUrl { get; set; }
+
+        [TempData]
+        public string? ErrorMessage { get; set; }
+    }
+}
